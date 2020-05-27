ed102292fb722fbf6014e157c9765176af058600
Anders Bjerner <abjerner@skybrud.dk>
2020-05-27 09:39:30 +0200

    Added JSON converter for BorgerDkMunicipality

---
+++ src/Skybrud.Integrations.BorgerDk/Json/BorgerDkJsonConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Skybrud.Integrations.BorgerDk.Json {
+
+    public class BorgerDkJsonConverter : JsonConverter {
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) {
+
+            BorgerDkMunicipality municipality = value as BorgerDkMunicipality;
+            
+            writer.WriteValue(municipality?.Code ?? 0);
+
+        }
+
+        public override object ReadJson(JsonReader reader, Type type, object existingValue, JsonSerializer serializer) {
+
+            if (type == typeof(BorgerDkMunicipality)) {
+
+                if (reader.TokenType == JsonToken.Integer) {
+                    return BorgerDkMunicipality.GetFromCode((int) (long) reader.Value);
+                }
+
+                return BorgerDkMunicipality.NoMunicipality;
+
+            }
+
+            throw new Exception("Unsupported type " + type);
+
+        }
+
+        public override bool CanConvert(Type type) {
+            return type == typeof(BorgerDkMunicipality);
+        }
+
+    }
+
+}
